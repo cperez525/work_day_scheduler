063fb3fef00feccc65696179255599110ea8ad7d
Cris Perez <cristino.perez525@gmail.com>
2020-08-09 14:48:54 -0500

    Finished styling elements on page

--- Reference_code/script.js
+++ Reference_code/script.js
@@ -1,18 +1,24 @@
-var workHours = ["8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM"]
+var workHours = ["8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM"]
+
 for (h = 0; h < workHours.length; h++) {
 
     var blockRow = $("<article>").addClass("row")
     var hourBlock = $("<section>").addClass("hour")
+    var hourText = $("<p>").addClass("hour-text")
     var timeBlock = $("<textarea>").addClass("time-block past")
     var saveBtn = $("<button>").addClass("saveBtn")
 
     console.log(workHours[h])
     $(".container").append(blockRow)
 
-    hourBlock.text(workHours[h])
+    hourBlock.append(hourText.text(workHours[h]))
 
-    timeBlock.attr("data-log", workHours[h])
+    blockRow.attr("data-log", workHours[h])
 
-    saveBtn.append("<img>").attr("src", "Images/save_icon.png")
+
     blockRow.append(hourBlock, timeBlock, saveBtn)
 }
+
+$(".saveBtn").on("click", function() {
+    
+})
